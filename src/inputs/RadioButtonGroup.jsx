bf5a25e180af1ee19ce5fb3a44f8a1042d0c5b4f
Kevin Suarez <ksuarez2@hotmail.com>
2018-03-26 20:59:43 -0500

    Change prop types to work with react v16.2.0

--- src/inputs/RadioButtonGroup.jsx
+++ src/inputs/RadioButtonGroup.jsx
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import { RadioButton, RadioButtonGroup as MaterialRadioButtonGroup } from 'material-ui/RadioButton';
 
 export default function RadioButtonGroup(props) {
--- src/inputs/Toggle.jsx
+++ src/inputs/Toggle.jsx
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import MaterialToggle from 'material-ui/Toggle';
 
 export default function Toggle(props) {
